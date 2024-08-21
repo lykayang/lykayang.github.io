375078c6780b1406c8eb13033de54c70e642c086
lykayang <null>
2024-08-21 08:31:55 +0000

    🎉auto update by Gmeek action

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ### :page_facing_up: [19](https://tiantian.cn/tag.html) 
 ### :speech_balloon: 0 
 ### :hibiscus: 48841 
-### :alarm_clock: 2024-08-21 14:30:56 
+### :alarm_clock: 2024-08-21 16:31:55 
 ### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)
--- docs/tiantian.js
+++ docs/tiantian.js
@@ -1,15 +1,18 @@
 const style = document.createElement("style");
 style.innerHTML = `
 
-.avatar {
-    width:64px;
-    height:64px;
-    object-fit: cover;
-}
-
 .avatar:hover {
     transform: scale(1) rotate(0deg);
 }
+
+.SideNav-item:hover {
+    background-color: #e2e2e2;
+}
+
+.SideNav-item {
+    border-color: transparent;
+}
+
 
 `;
 document.head.appendChild(style);
