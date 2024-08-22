6918c803862b0dc8f8022bc9a03f94299f1e7def
lykayang <lyka@163.com>
2024-08-22 16:25:48 +0800

    Add files via upload

--- static/tiantian.js
+++ static/tiantian.js
@@ -2,30 +2,22 @@ const style = document.createElement("style");
 style.innerHTML = `
 
 #avatarImg {
-    border: none !important;
-    outline: none !important;
-    box-shadow: none !important;
-    background-color: transparent !important;
+    border: none; /* 去掉边框 */
+    outline: none; /* 去掉边框 */
+    box-shadow: none; /* 去掉边框 */
+    background-color: transparent !important; /* 去掉底色 */
 }
 
 .avatar:hover {
-    transform: scale(1) rotate(0deg);
+    transform: scale(1) rotate(0deg); /* logo不要放大转动 */
 }
 
 .SideNav-item {
-    border-color: transparent;
-}
-
-
-#content {
-    border: 0 !important;
-    outline: 0 !important;
-    box-shadow: none !important;
-    background-color: transparent !important;
+    border-color: transparent; /* 去掉分隔线 */
 }
 
 nav.SideNav {
-    border: 0 !important;
+    border: 0 !important; /* 去掉边框 */
     outline: 0 !important;
     box-shadow: none !important;
     background-color: transparent !important;
